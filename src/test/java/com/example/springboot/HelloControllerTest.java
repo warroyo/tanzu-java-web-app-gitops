1f104a0dc2a231eed0834db47c9f30ad7f6705b3
Will Arroyo <warroyo7199008@gmail.com>
2024-03-21 11:11:31 -0600

    fix test

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -3,12 +3,15 @@ package com.example.springboot;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import javax.servlet.http.HttpServletRequest;
 
 @WebMvcTest(HelloController.class)
 class HelloControllerTest {
@@ -21,11 +24,12 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Greetings from Spring Boot + Tanzu multicloud!", controller.index());
+         MockHttpServletRequest request = new MockHttpServletRequest();
+        assertEquals("Greetings from Spring Boot + Tanzu multicloud! Clientt IP is 127.0.0.1", controller.index(request));
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Greetings from Spring Boot + Tanzu multicloud!"));
+            .andExpect(content().string("Greetings from Spring Boot + Tanzu multicloud! Clientt IP is 127.0.0.1"));
     }
 }
