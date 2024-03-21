a68bc31693b9ffbb741e4932614026ab6cebf594
Will Arroyo <warroyo7199008@gmail.com>
2024-03-21 10:47:43 -0600

    add client ip to response

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,17 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot + Tanzu multicloud!";
+	@GetMapping("/")
+	public String index(HttpServletRequest request) {
+		return "Greetings from Spring Boot + Tanzu multicloud! Clientt IP is "+  request.getRemoteAddr();
 	}
 
 }
